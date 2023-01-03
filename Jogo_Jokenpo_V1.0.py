4c6f6a88319e1907c0d2913cf848ab19ab1714cb
VonCrimson <renato.hsantos@hotmail.com>
2023-01-03 02:22:03 -0300

    adicionado as variavel Linha e Cores

--- Jogo_Jokenpo_V1.0.py
+++ Jogo_Jokenpo_V1.0.py
@@ -2,28 +2,35 @@ from random import choice
 from time import sleep
 from emoji import emojize
 
-# -> INSERIR CORES <-
+cores = {'limpa':'\033[m',
+         'vermelho':'\033[31m',
+         'amarelo':'\033[33m',
+         'verde':'\033[32m',
+         'ciano':'\033[36m',
+         'lilas':'\033[35m'}
 
-print(f'{" Seja bem vindo jogador! ":=^40}')
-print(f'{" Vamos jogar JOKENPO! ":=^40}')
+linhas = ('-='*20)
+
+print(f'{" Seja bem vindo jogador! ":^40}')
+print(f'{" Vamos jogar JOKENPO! ":^40}')
 sleep(1)
-print('-='*20)
+print(linhas)
 
-nome = str(input('Qual o seu nome jogador? ')).strip().capitalize()
-print('-='*20)
+nome = str(input(f"{(cores['ciano'])}Qual o seu nome jogador? {cores['limpa']}")).strip().capitalize()
+print(linhas)
 
 print(f'{"- INSTRUÇÕES -":^40}')
-print('-='*20)
+print(linhas)
 print(f'{"ESCOLHA ENTRE:":^40}')
-print(emojize(f'{"[:facepunch:]-Pedra [:hand:]-Papel [:v: ]-Tesoura!":^40}', language='alias')) #emoji
-print('-='*20)
+print(emojize(f'{"[:facepunch:]-Pedra [:hand:]-Papel [:v: ]-Tesoura!":^40}', language='alias')) 
+print(linhas)
 sleep(1)
 
 itens = ['Pedra','Papel','Tesoura']
 computador = choice(itens)
 
-jogador = str(input(f'Faça sua escolha {nome}: ')).strip().capitalize()
-print('-='*20)
+jogador = str(input(f"{(cores['ciano'])}Faça sua escolha {nome}: {cores['limpa']} ")).strip().capitalize()
+print(linhas)
 
 if jogador == 'Pedra' or jogador == 'Papel' or jogador == 'Tesoura':
 
@@ -33,7 +40,7 @@ if jogador == 'Pedra' or jogador == 'Papel' or jogador == 'Tesoura':
     sleep(1)
     print('PO!')
     sleep(1)
-    print('-='*20)
+    print(linhas)
     print(' ')
     print(f'- Computador jogou: {computador}') # Nesta parte, procurar possibilidades de inserir emojis nas escolhas de jogador/computador
     print(f'- {nome} jogou: {jogador}')
@@ -42,25 +49,25 @@ if jogador == 'Pedra' or jogador == 'Papel' or jogador == 'Tesoura':
 
     if computador == 'Pedra': 
             if jogador == 'Pedra':
-                print(emojize("EMPATAMOS! :sleepy:", language='alias'))
+                print(emojize(f"{cores['amarelo']}EMPATAMOS!{cores['limpa']} :sleepy:", language='alias'))
             elif jogador == 'Papel':
-                print(emojize(f"VOCÊ VENCEU {nome}! :trophy:", language='alias'))
+                print(emojize(f"{cores['verde']}VOCÊ VENCEU {nome}!{cores['limpa']} :trophy:", language='alias'))
             elif jogador == 'Tesoura':
-                print(emojize("VOCÊ PERDEU! :smiling_imp:", language='alias'))
+                print(emojize(f"{cores['vermelho']}VOCÊ PERDEU!{cores['limpa']} :smiling_imp:", language='alias'))
     elif computador == 'Papel':
             if jogador == 'Pedra':
-                print(emojize("VOCÊ PERDEU! :smiling_imp:", language='alias'))
+                print(emojize(f"{cores['vermelho']}VOCÊ PERDEU!{cores['limpa']} :smiling_imp:", language='alias'))
             elif jogador == 'Papel':
-                print(emojize("EMPATAMOS! :sleepy:", language='alias'))
+                print(emojize(f"{cores['amarelo']}EMPATAMOS!{cores['limpa']} :sleepy:", language='alias'))
             elif jogador == 'Tesoura':
-                print(emojize(f"VOCÊ VENCEU {nome}! :trophy:", language='alias'))
+                print(emojize(f"{cores['verde']}VOCÊ VENCEU {nome}!{cores['limpa']} :trophy:", language='alias'))
     elif computador == 'Tesoura':
         if jogador == 'Pedra':
-            print(emojize(f"VOCÊ VENCEU {nome}! :trophy:", language='alias'))
+            print(emojize(f"{cores['verde']}VOCÊ VENCEU {nome}!{cores['limpa']} :trophy:", language='alias'))
         elif jogador == 'Papel':
-            print(emojize("VOCÊ PERDEU! :smiling_imp:", language='alias'))
+            print(emojize(f"{cores['vermelho']}VOCÊ PERDEU!{cores['limpa']} :smiling_imp:", language='alias'))
         elif jogador == 'Tesoura':
-            print(emojize("EMPATAMOS! :sleepy:", language='alias'))
+            print(emojize(f"{cores['amarelo']}EMPATAMOS!{cores['limpa']} :sleepy:", language='alias'))
 else:
-    print(emojize(":x: OPÇÃO INVÁLIDA! TENTE NOVAMENTE! :x:", language='alias'))
-print('-='*20)
+    print(emojize(f"{cores['lilas']}:x: OPÇÃO INVÁLIDA! TENTE NOVAMENTE! :x:{cores['limpa']}", language='alias'))
+print(linhas)
